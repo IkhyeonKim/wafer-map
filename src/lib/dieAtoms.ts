1cc0f973d97b58e4af9bbe1dcdff5b50b6fa83c9
ikhyeon.kim <kih199212@gmail.com>
2025-07-03 19:05:39 -0400

    refactor: delete unused atoms

--- src/lib/dieAtoms.ts
+++ src/lib/dieAtoms.ts
@@ -3,19 +3,10 @@ import { Die } from "./Die"
 import { atom } from "jotai"
 import { stableIsDraggedAtom } from "@/components/Zoomable"
 
-export type DieAtom = Die & {
-	dieIndex: number
-	shouldMoveScroll: boolean
-}
-
 export const dieAtomFamily = atomFamily(
-	(dieInfo: DieAtom) => atom({ ...dieInfo }),
+	(dieInfo: Die) => atom({ ...dieInfo }),
 	(a, b) => a.id === b.id
 )
-
-export const prevSelectedDieAtom = atom<DieAtom | null>(null)
-
-export const currentSelectedDieAtom = atom<DieAtom | null>(null)
 
 export const selectedDieAtom = atom<Die | null>(null)
 
